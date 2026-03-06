68943498825d854578f0f632761edf479b9b5e84
Tanishq Garg <togargtanishq2003@gmail.com>
2026-03-07 00:15:52 +0530

    fix api base url

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,14 @@
+// import axios from 'axios'
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_BASE_URL
+// })
+
+// export default api
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export default api
